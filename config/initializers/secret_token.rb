850e6b81f086754370d8fe8f8456aeb3141aa9a5
AlexVuLe <vu.le.williams@gmail.com>
2013-02-09 00:59:59 -0500

    Create app for Project Zero

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectzero::Application.config.secret_token = '3006c78cd27d856954942b76e8afdd9ccc6e0e0daf08d792ee532bc0b3b9941e847fb632504b62bca03d620b7b4d97eae8c6f4950db0f9d2e645f791b6ab63ca'
